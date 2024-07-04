54900c2416b42416ef0051072dd1741ec1f87f06
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-07-04 10:19:01 +0530

    bug added

--- src/components/usestatedemo.js
+++ src/components/usestatedemo.js
@@ -7,7 +7,7 @@ function Counter() {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(count)}>Click me</button>
     </div>
   );
 }
